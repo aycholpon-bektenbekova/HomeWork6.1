16830427af6bc269ef1cb5f524f7f5814376e45e
DELL <aycholpon25t@gmail.com>
2023-01-22 11:27:43 +0600

    resultActivity

--- app/src/main/java/com/example/homework61/activities/SecondActivity.kt
+++ app/src/main/java/com/example/homework61/activities/SecondActivity.kt
@@ -1,15 +1,14 @@
 package com.example.homework61.activities
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.homework61.R
-import com.example.homework61.contracts.MainActivityContract
 import com.example.homework61.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
-    private val getResult = registerForActivityResult(MainActivityContract()) { result -> }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
@@ -25,7 +24,8 @@ class SecondActivity : AppCompatActivity() {
                         this@SecondActivity, getString(R.string.empty_text),
                         Toast.LENGTH_LONG).show()
                 } else {
-                    getResult.launch(binding.etSecond.text.toString())
+                    setResult(Activity.RESULT_OK)
+                    finish()
                 }
             }
         }
