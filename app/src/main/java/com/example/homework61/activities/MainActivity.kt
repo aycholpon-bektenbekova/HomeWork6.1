ee813596b34e3ff31bebc1e5b5381efaae310319
DELL <aycholpon25t@gmail.com>
2023-01-25 00:36:02 +0600

    resultActivity

--- app/src/main/java/com/example/homework61/activities/MainActivity.kt
+++ app/src/main/java/com/example/homework61/activities/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.homework61.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.homework61.R
-import com.example.homework61.contracts.SecondActivityContract
 import com.example.homework61.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val getResult = registerForActivityResult(SecondActivityContract()) { result ->}
+    private lateinit var getResult: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initLauncher()
         initClicker()
+    }
+
+    private fun initLauncher() {
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                binding.etFirst.setText(result.data?.getStringExtra(KEY))
+            }
+        }
     }
 
     private fun initClicker() {
@@ -26,9 +39,16 @@ class MainActivity : AppCompatActivity() {
                         Toast.LENGTH_LONG
                     ).show()
                 } else {
-                   getResult.launch(binding.etFirst.text.toString())
+                   Intent(this@MainActivity, SecondActivity::class.java).apply{
+                       putExtra(KEY, etFirst.text)
+                       getResult.launch(this)
+                   }
                 }
             }
         }
+    }
+
+    companion object{
+        const val KEY = "key for intent"
     }
 }
--- app/src/main/java/com/example/homework61/activities/SecondActivity.kt
+++ app/src/main/java/com/example/homework61/activities/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.homework61.activities
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -18,16 +19,21 @@ class SecondActivity : AppCompatActivity() {
 
     private fun initClicker() {
         with(binding) {
+            etSecond.setText(intent.getStringExtra(MainActivity.KEY))
             btnSecond.setOnClickListener {
                 if (binding.etSecond.text.isEmpty()) {
                     Toast.makeText(
                         this@SecondActivity, getString(R.string.empty_text),
-                        Toast.LENGTH_LONG).show()
+                        Toast.LENGTH_LONG
+                    ).show()
                 } else {
-                    setResult(Activity.RESULT_OK)
-                    finish()
+                    Intent(this@SecondActivity, MainActivity::class.java).apply {
+                        putExtra(MainActivity.KEY, etSecond.text)
+                        setResult(Activity.RESULT_OK)
+                    }
                 }
             }
         }
+        finish()
     }
 }
